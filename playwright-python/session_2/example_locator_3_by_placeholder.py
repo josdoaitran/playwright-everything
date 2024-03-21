b12a1cace31037486cb910e4fec0ed3b9af12e65
Doai Tran <josdoaitran@gmail.com>
2024-03-21 15:56:56 +0700

    example code with placeholder name

---
+++ playwright-python/session_2/example_locator_3_by_placeholder.py
@@ -0,0 +1,20 @@
+import asyncio
+from playwright.async_api import async_playwright, Playwright
+
+async def run(playwright: Playwright):
+    chromium = playwright.chromium # or "firefox" or "webkit".
+    browser = await chromium.launch(headless=False, slow_mo=500)
+    page = await browser.new_page()
+    await page.goto("https://www.saucedemo.com/")
+
+    ## Example get locator of element by placeholder
+    username_label = page.get_by_placeholder("Username")
+    await username_label.highlight()
+    await browser.close()
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+asyncio.run(main())
