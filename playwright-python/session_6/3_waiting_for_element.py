cd8e5e2b668e1330fd555435779c0e67f19c6d3e
Doai Tran <josdoaitran@gmail.com>
2024-04-04 10:42:34 +0700

    update example

--- playwright-python/session_6/3_waiting_for_element.py
+++ playwright-python/session_6/3_waiting_for_element.py
@@ -3,41 +3,17 @@ from playwright.sync_api import sync_playwright
 from time import perf_counter
 
 with sync_playwright() as playwright:
-
     browser = playwright.chromium.launch(headless=False, slow_mo=500)
     page = browser.new_page()
-    ## wait_until = load => load all contents of page: image, ...
-    # print ("=================================== START =======================================")
-    # start_time = perf_counter()
-    # # Load to page
-    # page.goto("https://demoqa.com/automation-practice-form", wait_until='load')
-    # # Loaded
-    # time_taken = perf_counter() - start_time
-    # print(f"Page is loaded  in {round(time_taken, 2)}")
-
-    ## wait_until = domcontentloaded => load html, dom only ... no wait all static content
-    # print("=================================== START =======================================")
-    # start_time = perf_counter()
-    # # Load to page
-    # page.goto("https://demoqa.com/automation-practice-form", wait_until = 'domcontentloaded')
-    # # Loaded
-    # time_taken = perf_counter() - start_time
-    # print(f"Page is loaded  in {round(time_taken, 2)}")
-
-    ## wait_until = commit => Send request to server and instantly receive response to do next
-    # print("=================================== START =======================================")
-    # start_time = perf_counter()
-    # # Load to page
-    # page.goto("https://demoqa.com/automation-practice-form", wait_until='commit')
-    # # Loaded
-    # time_taken = perf_counter() - start_time
-    # print(f"Page is loaded  in {round(time_taken, 2)}")
-
-    # wait_until = networkidle => wait untile network in browser idle
-    print("=================================== START =======================================")
+    page.goto("https://www.scrapethissite.com/pages/ajax-javascript/")
+    page.locator("//*[@id='2015']").click()
+    print("=================================== START WAIT=======================================")
     start_time = perf_counter()
-    # Load to page
-    page.goto("https://demoqa.com/automation-practice-form", wait_until='networkidle')
-    # Loaded
+    # title = page.locator("//*[@id='table-body']/tr/td[1]").first.click()
+
+    ########
+    title = page.locator("//*[@id='table-body']/tr/td[1]").first
+    title.wait_for(timeout=1500)
+    title.click()
     time_taken = perf_counter() - start_time
     print(f"Page is loaded  in {round(time_taken, 2)}")
--- playwright-python/session_6/readme.md
+++
@@ -1,1 +0,0 @@
-# In this session, Testing4Everyone gives you the example problems in writing and executing script
