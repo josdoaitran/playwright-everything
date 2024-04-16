cbcf0c890d91395be895a5efe9fae7269d7bb61f
Doai Tran <josdoaitran@gmail.com>
2024-04-16 23:46:53 +0700

    update tests

---
+++ playwright-python/session_9/reuse_authorize_sauce_demo.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import sync_playwright
+import os
+
+
+def delete_storage_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print("File deleted successfully.")
+    else:
+        print("The file does not exist.")
+
+
+with sync_playwright() as playwright:
+    browser = playwright.firefox.launch(headless=False, slow_mo=500)
+    # page = browser.new_page()
+    # page.goto("https://www.saucedemo.com/inventory.html")
+    # print(page.url)
+
+    context = browser.new_context(storage_state="./auth/storage_stage.json")
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/inventory.html")
+    page.pause()
+    print(page.url)
+
+    page.close()
+
+
