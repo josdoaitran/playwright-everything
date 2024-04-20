9687d2fdf949d418dcbae79b6088f76e0578c402
Doai Tran <josdoaitran@gmail.com>
2024-04-20 22:52:21 +0700

    generate test example fixture

--- playwright-python/session_10/basic_pytest_check_data/test_report_data_pytest_fixture.py
+++ playwright-python/session_10/basic_pytest_check_data/test_report_data_pytest_fixture.py
@@ -3,34 +3,22 @@ import pytest
 from session_10.basic_pytest_check_data.report_data import GeneratingReport
 
 
-def test_report_data():
-    GeneratingReport.generate_report("2024-4-20 12-37-9",
-                                     "Passed",
-                                     "TC-001 Covering Our test case example")
-    with open('report.json') as file:
-        data = json.load(file)
-        assert type(data) == dict
-        assert len(data) == 3
-def test_report_data_key():
-    GeneratingReport.generate_report("2024-4-20 12-37-9",
-                                     "Passed",
-                                     "TC-001 Covering Our test case example")
-    with open('report.json') as file:
-        data = json.load(file)
-        assert "timeStamp" in data
-        assert "status" in data
+def test_report_data(report_data):
+        assert type(report_data) == dict
+        assert len(report_data) == 3
 
-def test_report_data_value():
-    GeneratingReport.generate_report("2024-4-20 12-37-9",
-                                     "Passed",
-                                     "TC-001 Covering Our test case example")
-    with open('report.json') as file:
-        data = json.load(file)
-        assert "Passed" ==  data["status"]
-        assert "2024-4-20 12-37-9" == data["timeStamp"]
+def test_report_data_key(report_data):
+    assert "timeStamp" in report_data
+    assert "status" in report_data
+
+def test_report_data_value(report_data):
+    assert "Passed" ==  report_data["status"]
+    assert "2024-4-20 12-37-9" == report_data["timeStamp"]
 
 @pytest.fixture()
 def report_data():
-    GeneratingReport.generate_report()
+    GeneratingReport.generate_report("2024-4-20 12-37-9",
+                                     "Passed",
+                                     "TC-001 Covering Our test case example")
     with open("report.json") as file:
         return json.load(file)
