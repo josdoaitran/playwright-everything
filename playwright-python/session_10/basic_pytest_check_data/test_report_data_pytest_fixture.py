498f2f1a3d4af073c249ae971ded692452888a07
Doai Tran <josdoaitran@gmail.com>
2024-04-20 22:58:28 +0700

    update example code

--- playwright-python/session_10/basic_pytest_check_data/test_report_data_pytest_fixture.py
+++ playwright-python/session_10/basic_pytest_check_data/test_report_data_pytest_fixture.py
@@ -4,19 +4,23 @@ from session_10.basic_pytest_check_data.report_data import GeneratingReport
 
 
 def test_report_data(report_data):
-        assert type(report_data) == dict
-        assert len(report_data) == 3
+    print("\n Verify Test Case 01")
+    assert type(report_data) == dict
+    assert len(report_data) == 3
 
 def test_report_data_key(report_data):
+    print("\n Verify Test Case 02")
     assert "timeStamp" in report_data
     assert "status" in report_data
 
 def test_report_data_value(report_data):
+    print("\n Verify Test Case 03")
     assert "Passed" ==  report_data["status"]
     assert "2024-4-20 12-37-9" == report_data["timeStamp"]
 
 @pytest.fixture()
 def report_data():
+    print("\n Generate report in pytest fixture")
     GeneratingReport.generate_report("2024-4-20 12-37-9",
                                      "Passed",
                                      "TC-001 Covering Our test case example")
