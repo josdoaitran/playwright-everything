6d8bed329e000911b62d774c2842fc60a2774d86
Doai Tran <josdoaitran@gmail.com>
2024-04-20 10:28:43 +0700

    update tessts example @pytest.fixture()

--- playwright-python/session_10/basic_pytest_check_data/test_report_data.py
+++ playwright-python/session_10/basic_pytest_check_data/test_report_data.py
@@ -1,5 +1,7 @@
 import json
+import pytest
 from session_10.basic_pytest_check_data.report_data import GeneratingReport
+
 
 def test_report_data():
     GeneratingReport.generate_report()
@@ -8,4 +10,11 @@ def test_report_data():
         assert type(data) == dict
         assert len(data) == 3
         assert "timeStamp" in data
+        assert "status" in data
 
+
+@pytest.fixture()
+def report_data():
+    GeneratingReport.generate_report()
+    with open("report.json") as file:
+        return json.load(file)
