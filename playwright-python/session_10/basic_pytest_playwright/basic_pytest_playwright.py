60ad111608700e8a4451082796d2451e7eda3089
Doai Tran <josdoaitran@gmail.com>
2024-04-21 15:11:52 +0700

    update test frameworks

--- playwright-python/session_10/basic_pytest_playwright/basic_pytest_playwright.py
+++ playwright-python/session_10/basic_pytest_playwright/basic_pytest_playwright.py
@@ -1,16 +1,25 @@
 from playwright.sync_api import Page, sync_playwright
 import pytest
+import logging
 
+# Create a named logger
+logger = logging.getLogger('__Test Login__')
+logger.setLevel(logging.INFO)
 
 @pytest.fixture()
 def page():
+    logger.info(msg="Configure Our Project")
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=False, slow_mo=500)
     return browser.new_page()
 
 
 def test_website(page: Page):
+    logger.info(msg="Open Browser")
     page.goto("https://www.saucedemo.com/")
+    logger.info(msg="Input Username")
     page.locator("//input[@id='user-name']").fill("standard_user")
+    logger.info(msg="Input password")
     page.locator("//input[@id='password']").fill("secret_sauce")
     page.locator("//input[@id='login-button']").click()
+    logger.info(msg="Connect login")
