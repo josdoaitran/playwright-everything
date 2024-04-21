0bbaa4e20fd5972c12768e929738d3402b8b43e3
Doai Tran <josdoaitran@gmail.com>
2024-04-22 00:27:27 +0700

    update tests chapter 9

--- playwright-python/session_10/basic_pytest_playwright/basic_pytest_playwright.py
+++ playwright-python/session_10/basic_pytest_playwright/basic_pytest_playwright.py
@@ -16,31 +16,34 @@ def browser_context_args(browser_context_args):
     }
 
 @pytest.fixture(scope="function", autouse=True)
-def set_up_browser(page: Page):
+def set_up_page(page: Page):
     logger.info(msg="Configure Our Project")
     # playwright = sync_playwright().start()
     # browser = playwright.chromium.launch(headless=False, slow_mo=500)
     # page = browser.new_page()
     logger.info(msg="Open Browser")
     page.goto("https://www.saucedemo.com/")
-    yield
-
-def test_website_login_standard_user(page: Page):
-    logger.info(msg="Input Username")
-    page.locator("//input[@id='user-name']").fill("standard_user")
-    logger.info(msg="Input password")
-    page.locator("//input[@id='password']").fill("secret_sauce")
-    logger.info(msg="Connect login")
-    page.locator("//input[@id='login-button']").click()
-    assert page.locator("//div[@class='app_logo']").is_visible()
+    # return page
+    yield page
+    logger.info(msg="Close Browser")
     page.close()
 
-def test_website_login_performance_glitch_user(page: Page):
+def test_website_login_standard_user(set_up_page: Page):
     logger.info(msg="Input Username")
-    page.locator("//input[@id='user-name']").fill("performance_glitch_user")
+    set_up_page.locator("//input[@id='user-name']").fill("standard_user")
     logger.info(msg="Input password")
-    page.locator("//input[@id='password']").fill("secret_sauce")
+    set_up_page.locator("//input[@id='password']").fill("secret_sauce")
     logger.info(msg="Connect login")
-    page.locator("//input[@id='login-button']").click()
-    assert page.locator("//div[@class='app_logo']").is_visible()
-    page.close()
+    set_up_page.locator("//input[@id='login-button']").click()
+    assert set_up_page.locator("//div[@class='app_logo']").is_visible()
+    # set_up_page.close()
+
+def test_website_login_performance_glitch_user(set_up_page: Page):
+    logger.info(msg="Input Username")
+    set_up_page.locator("//input[@id='user-name']").fill("performance_glitch_user")
+    logger.info(msg="Input password")
+    set_up_page.locator("//input[@id='password']").fill("secret_sauce")
+    logger.info(msg="Connect login")
+    set_up_page.locator("//input[@id='login-button']").click()
+    assert set_up_page.locator("//div[@class='app_logo']").is_visible()
+    # set_up_page.close()
