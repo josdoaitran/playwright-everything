0621d3be21a20b48dfb8f5e3e09042672c6165cc
Doai Tran <josdoaitran@gmail.com>
2024-04-24 00:05:26 +0700

    update code

--- playwright-python/session_10/basic_pytest/pytest_example_method.py
+++ playwright-python/session_10/basic_pytest/pytest_example_method.py
@@ -1,22 +1,22 @@
 from session_10.basic_pytest.example_method import ExampleCode
 # generate pytest to cover ExampleCode.average on example_method
 def test_example_average_method():
-    assert (ExampleCode.average(1, 3), 2)
-    assert (ExampleCode.average(1, 2), 1.5)
-    assert (ExampleCode.average(1, 1), 1)
-    assert (ExampleCode.average(1, 0), 0.5)
-    assert (ExampleCode.average(1, -1), 0)
-    assert (ExampleCode.average(1, -2), -0.5)
-    assert (ExampleCode.average(1, -3), -1)
-    assert (ExampleCode.average(1, -4), -1.5)
-    assert (ExampleCode.average(1, -5), -2)
-    assert (ExampleCode.average(1, -6), -2.5)
+    assert ExampleCode.average(1, 3) == 2
+    assert ExampleCode.average(1, 2)== 1.5
+    assert ExampleCode.average(1, 1)== 1
+    assert ExampleCode.average(1, 0) == 0.5
+    assert ExampleCode.average(1, -1) == 0
+    assert ExampleCode.average(1, -2) == -0.5
+    assert ExampleCode.average(1, -3)== -1
+    assert ExampleCode.average(1, -4) == -1.5
+    assert ExampleCode.average(1, -5) == -2
+
     
 def test_example_sum_method():
-    assert (ExampleCode.sum(1, 3), 4)
-    assert (ExampleCode.sum(1, 2), 3)
-    assert (ExampleCode.sum(1, 1), 2)
-    assert (ExampleCode.sum(1, 0), 1)
-    assert (ExampleCode.sum(1, -1), 0)
+    assert ExampleCode.sum(1, 3) == 4
+    assert ExampleCode.sum(1, 2) == 3
+    assert ExampleCode.sum(1, 1) == 2
+    assert ExampleCode.sum(1, 0) == 1
+    assert ExampleCode.sum(1, -1) == 0
 
 
