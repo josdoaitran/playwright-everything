0cca8830f3028425473a63cd59a5d7abb8a4861e
Doai Tran <josdoaitran@gmail.com>
2024-04-26 21:23:11 +0700

    upate example

--- playwright-python/session_12/basic_record_video/record_video.py
+++ playwright-python/session_12/basic_record_video/record_video.py
@@ -1,5 +1,4 @@
-from playwright.sync_api import Page, sync_playwright
-import pytest
+from playwright.sync_api import Page, Browser
 import logging
 import os
 
@@ -7,57 +6,36 @@ import os
 logger = logging.getLogger('__Test Login__')
 logger.setLevel(logging.INFO)
 
-
-def delete_storage_file(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        logger.info("File deleted successfully.")
-    else:
-        logger.info("The file does not exist.")
-
-
 # configure igonore ssl
 # refer to: https://playwright.dev/python/docs/test-runners#ignore-https-errors
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "ignore_https_errors": True
-    }
 
-@pytest.fixture(scope="function", autouse=True)
-def set_up_page(page: Page):
-    logger.info(msg="Configure Our Project")
-    # playwright = sync_playwright().start()
-    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    # page = browser.new_page()
-    logger.info(msg="Open Browser")
+
+def test_website_login_standard_user(browser: Browser):
+    context = browser.new_context(
+        record_video_dir="video/"
+    )
+    page = context.new_page()
     page.goto("https://www.saucedemo.com/")
-    # return page
-    yield page
-    logger.info(msg="Close Browser")
-    page.close()
-
-def test_website_login_standard_user(set_up_page: Page):
-    delete_storage_file("screen_shoot/screen_shoot/website_login_standard_user.png")
     logger.info(msg="Input Username")
-    set_up_page.locator("//input[@id='user-name']").fill("standard_user")
+    page.locator("//input[@id='user-name']").fill("standard_user")
     logger.info(msg="Input password")
-    set_up_page.locator("//input[@id='password']").fill("secret_sauce")
+    page.locator("//input[@id='password']").fill("secret_sauce")
     logger.info(msg="Connect login")
-    set_up_page.locator("//input[@id='login-button']").click()
-    assert set_up_page.locator("//div[@class='app_logo']").is_visible()
-    set_up_page.screenshot(path="screen_shoot/screen_shoot/website_login_standard_user.png")
+    page.locator("//input[@id='login-button']").click()
+    assert page.locator("//div[@class='app_logo']").is_visible()
     # set_up_page.close()
 
-def test_website_login_performance_glitch_user(set_up_page: Page):
-    delete_storage_file("screen_shoot/screen_shoot/website_login_performance_glitch_use.png")
+def test_website_login_performance_glitch_user(browser: Browser):
+    context = browser.new_context(
+        record_video_dir="video/"
+    )
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
     logger.info(msg="Input Username")
-    set_up_page.locator("//input[@id='user-name']").fill("performance_glitch_user")
+    page.locator("//input[@id='user-name']").fill("performance_glitch_user")
     logger.info(msg="Input password")
-    set_up_page.locator("//input[@id='password']").fill("secret_sauce")
+    page.locator("//input[@id='password']").fill("secret_sauce")
     logger.info(msg="Connect login")
-    set_up_page.locator("//input[@id='login-button']").click()
-    assert set_up_page.locator("//div[@class='app_logo']").is_visible()
-    set_up_page.screenshot(path="screen_shoot/screen_shoot/website_login_performance_glitch_use.png", full_page=True)
+    page.locator("//input[@id='login-button']").click()
+    assert page.locator("//div[@class='app_logo']").is_visible()
     # set_up_page.close()
