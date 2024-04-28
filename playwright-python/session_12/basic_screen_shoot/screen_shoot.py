ec3ba8dd8ea5288538acb266300ec4559b54a291
Doai Tran <josdoaitran@gmail.com>
2024-04-28 15:22:54 +0700

    update run tests

--- playwright-python/session_12/basic_screen_shoot/screen_shoot.py
+++ playwright-python/session_12/basic_screen_shoot/screen_shoot.py
@@ -15,22 +15,9 @@ def delete_storage_file(file_path):
     else:
         logger.info("The file does not exist.")
 
-
-# configure igonore ssl
-# refer to: https://playwright.dev/python/docs/test-runners#ignore-https-errors
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {
-        **browser_context_args,
-        "ignore_https_errors": True
-    }
-
 @pytest.fixture(scope="function", autouse=True)
 def set_up_page(page: Page):
     logger.info(msg="Configure Our Project")
-    # playwright = sync_playwright().start()
-    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    # page = browser.new_page()
     logger.info(msg="Open Browser")
     page.goto("https://www.saucedemo.com/")
     # return page
@@ -48,16 +35,15 @@ def test_website_login_standard_user(set_up_page: Page):
     set_up_page.locator("//input[@id='login-button']").click()
     assert set_up_page.locator("//div[@class='app_logo']").is_visible()
     set_up_page.screenshot(path="./screenshoot/website_login_standard_user.png")
-    # set_up_page.close()
 
 def test_website_login_performance_glitch_user(set_up_page: Page):
     delete_storage_file("./screenshoot/website_login_performance_glitch_use.png")
     logger.info(msg="Input Username")
-    page.locator("//input[@id='user-name']").fill("performance_glitch_user")
+    set_up_page.locator("//input[@id='user-name']").fill("performance_glitch_user")
     logger.info(msg="Input password")
-    page.locator("//input[@id='password']").fill("secret_sauce")
+    set_up_page.locator("//input[@id='password']").fill("secret_sauce")
     logger.info(msg="Connect login")
-    page.locator("//input[@id='login-button']").click()
-    assert page.locator("//a[@class='shopping_cart_link']").is_visible()
+    set_up_page.locator("//input[@id='login-button']").click()
+    assert set_up_page.locator("//a[@class='shopping_cart_link']").is_visible()
     set_up_page.screenshot(path="./screenshoot/website_login_performance_glitch_use.png", full_page=True)
 
