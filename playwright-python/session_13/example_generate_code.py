f5ca066281e5edc6778aaccaef6b22eebe478702
Doai Tran <josdoaitran@gmail.com>
2024-05-02 18:14:25 +0700

    update code

--- playwright-python/session_13/example_generate_code.py
+++ playwright-python/session_13/example_generate_code.py
@@ -9,15 +9,11 @@ async def run(playwright: Playwright) -> None:
     page = await context.new_page()
     await page.goto("https://www.saucedemo.com/")
     await page.locator("[data-test=\"username\"]").click()
-    await page.locator("[data-test=\"username\"]").click()
     await page.locator("[data-test=\"username\"]").fill("standard_user")
     await page.locator("[data-test=\"password\"]").click()
     await page.locator("[data-test=\"password\"]").fill("secret_sauce")
     await page.locator("[data-test=\"login-button\"]").click()
-    await page.get_by_text("$29.99").click()
-    await page.locator("[data-test=\"item-4-title-link\"]").click()
-    await page.locator("[data-test=\"add-to-cart\"]").click()
-    await page.locator("[data-test=\"back-to-products\"]").click()
+    await page.locator("[data-test=\"product-sort-container\"]").select_option("za")
 
     # ---------------------
     await context.close()
