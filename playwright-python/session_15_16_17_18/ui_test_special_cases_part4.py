93269f1733db2ee7356304f229b8465e129975f0
Doai Tran <josdoaitran@gmail.com>
2024-05-07 15:26:10 +0700

    update tests examples

--- playwright-python/session_15_16_17_18/ui_test_special_cases_part4.py
+++ playwright-python/session_15_16_17_18/ui_test_special_cases_part4.py
@@ -5,5 +5,17 @@ from playwright.sync_api import Page, expect
 # Example: http://www.uitestingplayground.com/progressbar
 def test_verify_progress_bar(page: Page):
     page.goto("http://www.uitestingplayground.com/progressbar")
-
+    start_btn = page.locator("button.btn-primary")
+    stop_btn = page.locator("button.btn-info")
+    progres_bar = page.locator("div.progress-bar")
+    start_btn.click()
+    while True:
+        value = int(progres_bar.get_attribute("aria-valuenow"))
+        if value >= 75:
+            break
+        else:
+            print(f"Percentange: {value}%")
+    stop_btn.click()
+    expect(progres_bar).to_have_attribute("aria-valuenow", "75")
+    assert value >= 75
 
