46264a477bc7fe00f9fd8996a6eb5719cec0f2f4
Doai Tran <josdoaitran@gmail.com>
2024-05-07 23:14:01 +0700

    update tests

--- playwright-python/session_15_16_17_18/ui_test_special_cases_part5.py
+++ playwright-python/session_15_16_17_18/ui_test_special_cases_part5.py
@@ -12,3 +12,26 @@ def test_example_mouse_over(page: Page):
     text_warming.click(click_count=4)
     expect(page.locator("//span[@id='clickCount']")).to_have_text("4")
 
+# 14. Special Case: Non-breaking space
+def test_example_non_breaking_space(page: Page):
+    page.goto("http://www.uitestingplayground.com/nbsp")
+    # page.locator("//button[text()='My Button']").click(timeout=2000)
+    page.locator("//button[text()='My&nbsp;Button']").click(timeout=2000)
+
+# 15. Overlapped element
+# http://www.uitestingplayground.com/overlapped
+def test_example_overlapped_element(page: Page):
+    page.goto("http://www.uitestingplayground.com/overlapped")
+    name_text_bot = page.get_by_placeholder("Name")
+    # name_text_bot.scroll_into_view_if_needed()
+    # name_text_bot.fill("Testing4Everyone")
+    # expect(name_text_bot).to_have_value("Testing4Everyone")
+
+    # div = page.locator("//div[@style='overflow-y: scroll; height:100px;']")
+    div = name_text_bot.locator("..")
+    div.hover()
+    page.mouse.wheel(0,200)
+    name_text_bot.click()
+    name_text_bot.fill("Testing4Everyone")
+    expect(name_text_bot).to_have_value("Testing4Everyone")
+
