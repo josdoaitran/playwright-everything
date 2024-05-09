e0e942db4b948eb9efb224f76d9c6d5b30e6c32b
Doai Tran <josdoaitran@gmail.com>
2024-05-10 00:06:41 +0700

    update tests

--- playwright-python/session_20/browser_selection_example.py
+++ playwright-python/session_20/browser_selection_example.py
@@ -5,6 +5,14 @@ from playwright.sync_api import Page
 logger = logging.getLogger('__Test Login__')
 logger.setLevel(logging.INFO)
 
+@pytest.fixture(scope="function", autouse=True)
+def set_up_page(page: Page):
+    logger.info(msg="Open Browser")
+    page.goto("https://www.saucedemo.com/")
+    yield page
+    logger.info(msg="Close Browser")
+    page.close()
+
 @pytest.mark.skip_browser("firefox")
 def test_website_login_standard_user(page: Page) -> None:
     logger.info(msg="Input Username")
