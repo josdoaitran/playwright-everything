f5f119321adbe838ae9b438f7064d15a048db227
Doai Tran <josdoaitran@gmail.com>
2024-05-13 13:22:48 +0700

    update tests

--- playwright-python/session_21/test_page_object_model_example.py
+++ playwright-python/session_21/test_page_object_model_example.py
@@ -19,7 +19,6 @@ def test_login_tc_01(page: Page):
 
 def test_login_tc_02(page: Page):
     login_page = LoginPage(page)
-    iventory_page = InventoryPage(page)
     login_page.login("standard_user1", "secret_sauce")
     login_page.verify_login_failed_alert_message()
 
